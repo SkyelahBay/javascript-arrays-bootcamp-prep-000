12d590795567d5930af812ee10d8e12cc1d509b0
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-11 01:30:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,15 +10,17 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 
 function addElementToBeginningOfArray(arrayName, elementToAdd){
-  return [elementToAdd, ...arrayName];
+  let newArray = [elementToAdd, ...arrayName];
+  return newArray;
 }
 function destructivelyAddElementToBeginningOfArray(arrayName, elementToAdd){
   return arrayName.unshift(elementToAdd);
 }
 
 function addElementToEndOfArray(arrayName, elementToAdd){
-  return [arrayName, ...elementToAdd];
+  let newArray = [arrayName, ...elementToAdd];
+  return newArray;
 }
 function destructivelyAddElementToEndOfArray(arrayName, elementToAdd){
-  return arrayName[arrayName.length] = elementToAdd;
+  return arrayName.push(elementToAdd);
 }
