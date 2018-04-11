19083ceebe68a0ea1280b695498efd34c58c5358
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-11 01:14:26 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,12 @@
+/*
+let grilledCheeseIngredients  = [
+  'bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic'
+];
+let tomatoSauceIngredients = [
+  'tomato', 'garlic', 'olive oil', 'basil', 'oregano'
+];
+*/
+let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(arrayName, elementToAdd){
+  
+}
